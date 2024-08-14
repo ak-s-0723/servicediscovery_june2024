c16726f665c788928e36c09cafa141d921947a5f
khannaanurag1 <khannaanurag1@gmail.com>
2024-08-14 09:33:29 +0530

    ServiceDiscovery - EurekaServer

---
+++ src/main/java/org/example/servicediscovery_june2024/ServiceDiscoveryJune2024Application.java
@@ -0,0 +1,13 @@
+package org.example.servicediscovery_june2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryJune2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceDiscoveryJune2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/servicediscovery_june2024/ServiceDiscoveryJune2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.servicediscovery_june2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryJune2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
